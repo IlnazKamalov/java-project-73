efb956c11d44cccc63fd7a7a3100fb6fe3974a0f
IlnazKamalov <kamalovpk@gmail.com>
2023-01-18 18:57:04 +0300

    update and fix change-log & AppApplication add index.html

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 public class AppApplication {
 
     public static void main(String[] args) {
