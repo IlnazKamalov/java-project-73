24ec8518d0b55958de6e2481fdc1978948c20b79
IlnazKamalov <kamalovpk@gmail.com>
2023-01-18 12:19:36 +0300

    step 2

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,54 @@
+package hexlet.code.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Temporal;
+import javax.persistence.Column;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+//    public User(Long id) {
+//        this.id = id;
+//    }
+}
